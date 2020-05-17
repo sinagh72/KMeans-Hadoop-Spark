8e351623f999605bb010752253b9ee1444629703
sinagh72 <sina.gholami72@gmail.com>
2020-05-17 18:49:52 +0200

    creating directories

---
+++ hadoop/k-means/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $unipi.it.k_means;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
