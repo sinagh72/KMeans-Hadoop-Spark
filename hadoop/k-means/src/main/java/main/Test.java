22e602632b431e2698819510966f0d20ab9a56a3
sinagh72 <sina.gholami72@gmail.com>
2020-05-21 12:57:16 +0430

    test file updated

--- hadoop/k-means/src/main/java/main/Test.java
+++ hadoop/k-means/src/main/java/main/Test.java
@@ -1,11 +1,7 @@
 package main;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Random;
-import java.util.stream.Stream;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
@@ -18,18 +14,6 @@ public class Test {
 //		d.set(0, -1);
 //		d.add(23);
 //		System.out.println(z.toString().replace("[", "").replace("]", ""));
-		Random r = new Random();
-
-		d.forEach(i -> {
-			String[] line;
-			try (Stream<String> lines = Files.lines(Paths.get("file.txt"))) {
-				line = lines.skip(i).findFirst().get().trim().split(",");
-				System.out.println(line);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		});
 
 	}
 
