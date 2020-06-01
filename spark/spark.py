54b0a6f9767b496aaadf06d8f208d5bb2fd9605c
lorenzogio97 <45814828+lorenzogio97@users.noreply.github.com>
2020-06-01 14:46:30 +0200

    k-means implemented, need testing and output of result

--- spark/spark.py
+++ spark/spark.py
@@ -6,65 +6,111 @@ from pyspark import SparkContext
 def create_vector(line):
     vector= []
     vector= line.split(",")
-    return vector
+    for i in range(len(vector)):
+        vector[i] = float(vector[i])
+    return (int(vector[0]), vector[1:])
 
 def select_random_centroid(point, indexes):
     if point[0] in indexes:
-        return point
+        return [point] #key, value
+    return []
 
-def assign_to_cluster(point, centroid): 
-    #compute the distance from each centroid and output the key corrisponing to the nearest 
-    print("tets")
+def assign_to_cluster(point, centroids):
+    #compute the distance from each centroid and output the key corrisponing to the nearest
+    idx=0
+    min_dist=float("inf")
+    for centroid in centroids:
+        dist=distance(point[1], centroid[1]) #they are key-value element
+        if dist<min_dist:
+            min_dist= dist
+            idx=centroids.index(centroid)
+    return (idx, point)
+
+
+def distance (p1, p2):
+    sum=0
+    if len(p1)!=len(p2):
+        print("Error computing the distance: different dimension")
+
+    for iteration in range(len(p1)):
+        dist= p1[iteration]-p2[iteration]
+        sum += (dist**2)
+    return (sum**(1/2)) #return the index of the centroid and the distance from the point
+
+
+def vector_sum (key_value): #here is (k, list of points in key_value form)
+    key=key_value[0]
+    points= key_value[1]
+    count= len(points)
+    new_centroid=[sum(x)/count for x in zip(*[y[1] for y in points])]
+    return (key, new_centroid)
+
+
+
 
 if __name__ == "__main__":
-    if len(sys.argv) !=6 :
+    if len(sys.argv) !=7 :
         print("Usage: K-means <k> <rows> <columns> <threshold> <input file> <output file>", file=sys.stderr)
         sys.exit(-1)
-#parameter from the command line 
-    k = sys.argv[1]
-    rows= sys.argv[2]
-    columns = sys.argv[3]
-    threshold =sys.argv[4]
+
+#parameter from the command line
+    k = int(sys.argv[1])
+    rows= int(sys.argv[2])
+    columns = int(sys.argv[3])
+    threshold =float(sys.argv[4])
     input_file= sys.argv[5]
     output_file= sys.argv[6]
 
-    print("Number of cluster: "+ k)
-    print("Number of vector: " + rows)
-    print("Dimension of vectors: "+ columns)
-    print("Threshold: "+  threshold)
+    print("Number of cluster: "+ str(k))
+    print("Number of vector: " + str(rows))
+    print("Dimension of vectors: "+ str(columns))
+    print("Threshold: "+  str(threshold))
     print("Input file: "+input_file)
     print("Output file: "+ output_file)
-    
 
-#initialize spark context    
+
+#initialize spark context
     master = "local"
     sc = SparkContext(master, "K-means")
-
+    sc.setLogLevel("WARN")
     #set a maximum number of iteration
-    iteration= 100
+    iteration= 3
 
     lines = sc.textFile(sys.argv[5])
 
-    datapoints=lines.flatMap(lambda point : create_vector(point))
-
+    datapoints=lines.map(lambda point : create_vector(point))
     #select random indexes
     indexes= random.sample(range(rows), k)
 
-    centroid = lines.flatMap(lambda point : select_random_centroid(point, indexes))
+    #extract centroid from the datapoint
+    centroids = datapoints.flatMap(lambda point : select_random_centroid(point, indexes)).collect()
     finish= False
 
-    while iteration>0 or not finish:
-        data_assigned = datapoints.map(lambda point : assign_to_cluster(point, centroid) ) #this should return a (assigned_cluster, datapoint) pair 
+    print("CENTROID_RANDOM: ", centroids)
+    print("INDICI: ",indexes)
 
-        data_assigned.reduceByKey(...) #calculate the new centroid summing each component and dividing the number
+    while iteration>0 and not finish:
+        data_assigned = datapoints.map(lambda point : assign_to_cluster(point, centroids) ) #this should return a (assigned_cluster, datapoint) pair
+        groupped= data_assigned.groupByKey().mapValues(list)
+        print("GROUPPED: ",groupped.collect())
+        new_centroids= groupped.map(lambda rdd: vector_sum(rdd)).collect()
 
-        #compare between two iteration and set condition "finish"
+        finish=True
 
-    #save the centroid (how to consider the centroid that are not point?)
+        print("ITERAZIONE ",iteration,": ",  new_centroids)
+
+        #stop condition
+        for i in range(k):
+            if distance(new_centroids[i][1], centroids[i][1])>threshold:
+                finish=False
+
+        centroids=new_centroids
+
+        iteration-=1
+        #print(data_assigned.collect())
+        #print(groupped.collect())
+        #print(new_centroid.collect())
 
 
 
-
-
-    
-
+    print(centroids)
